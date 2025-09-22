64adbf04f4e433f52311087520f954f39f454c32
PauloCN01 <paulo.ferreira3@estudante.edu.br>
2025-09-22 09:40:36 -0400

    09-22 usuario(em produção)

---
+++ src/usuarios/usuario.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+class UsuarioController {
+  adicionar(req:Request, res:Response) {
+    
+  }
+  listar(req:Request, res:Response) {
+    
+  }
+}
+export default new UsuarioController();
