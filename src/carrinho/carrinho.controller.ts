9872822d9bc53c0834e67d06669f587725fde7a7
PauloCN01 <paulo.ferreira3@estudante.edu.br>
2025-10-06 09:57:37 -0400

    10-06 carrinho(adicionarItem feito)- terminar o resto

--- src/carrinho/carrinho.controller.ts
+++ src/carrinho/carrinho.controller.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from 'express';
 import { db } from '../database/banco.mongo.js';
+import { ObjectId } from 'mongodb';
 
 interface ItemCarrinho {
-  produtoId: number;
+  produtoId: ObjectId;
   quantidade: number;
-  precosolitario: number;
+ precoUnitario: number;
   nome: string;
 }
 
 interface Carrinho {
-  usuarioId: number;
+  usuarioId: ObjectId;
   itens: ItemCarrinho[];
   datatualizacao: Date;
   total: number;
@@ -18,32 +19,36 @@ interface Carrinho {
 class CarrinhoController {
     //adicionarItem
     async adicionarItem(req: Request, res: Response) {
-        const { usuarioId, produtoId, quantidade, precosolitario, nome } = req.body;
-        console.log(usuarioId, produtoId, quantidade, precosolitario, nome);
+        const { usuarioId, produtoId, quantidade, precoUnitario, nome } = req.body;
+        console.log(usuarioId, produtoId, quantidade, precoUnitario, nome);
         
         // Verificar se o carrinho do usuário já existe
-        const carrinho = await db.collection<Carrinho>('carrinhos').find({ usuarioID: usuarioId }).toArray();
-        if (carrinho.length === 0) {
+        const carrinho = await db.collection<Carrinho>('carrinhos').findOne({ usuarioID: ObjectId.createFromHexString(usuarioId) });
+        // Se não existir, criar um novo carrinho
+        if (!carrinho) {
           const novoCarrinho: Carrinho = {
-            usuarioId,
-            itens: [{ produtoId, quantidade, precosolitario, nome }],
+            usuarioId: ObjectId.createFromHexString(usuarioId),
+            itens: [{ produtoId, quantidade, precoUnitario, nome }],
             datatualizacao: new Date(),
-            total: quantidade * precosolitario
+            total: quantidade * precoUnitario
           } 
           await db.collection<Carrinho>('carrinhos').insertOne(novoCarrinho);
           res.status(201).json(novoCarrinho);
         }
         else {
-          
-        }      
-        // Se não existir, criar um novo carrinho
-        // Se não existir, criar um novo carrinho
+          //verificar se o item já existe no carrinho
+          const itemExiste = carrinho.itens.find((item) => item.produtoId = ObjectId.createFromHexString(produtoId));
+          if (!itemExiste) {
+            carrinho.itens.push({ produtoId: ObjectId.createFromHexString(produtoId), quantidade, precoUnitario, nome });
+            // calcular o total do carrinho
+            carrinho.total += quantidade * precoUnitario;
+            // Atualizar a data de atualização do carrinho
+            carrinho.datatualizacao = new Date();
+            await db.collection<Carrinho>('carrinhos').updateOne({ usuarioId: ObjectId.createFromHexString(usuarioId) }, { $set:{itens: carrinho.itens, total: carrinho.total, datatualizacao: carrinho.datatualizacao, } });
+            res.status(200).json(carrinho);
+          }
+        }
 
-        // Se existir, deve adicionar item no carrinho
-
-        // calcular o total do carrinho
-
-        // Atualizar a data de atualização do carrinho
     }
 
     //removerItem
