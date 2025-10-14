795bf6d3d68312bd7006c43bfcb6cd3526104499
PauloCN01 <paulo.ferreira3@estudante.edu.br>
2025-10-14 11:50:44 -0400

    10-14-autenticação token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,13 +1,30 @@
 //Explicando o que é um middleware
+import jwt from 'jsonwebtoken'
 import {Request, Response, NextFunction} from 'express'
-function Auth(req:Request,res:Response,next:NextFunction){
+
+interface RequestAuth extends Request{
+    usuarioId?: string
+}
+
+function Auth(req:RequestAuth,res:Response,next:NextFunction){
     const authHeader = req.headers.authorization
     if(!authHeader)
         return res.status(401).json({mensagem:"Cadê o Token?"})
-    const [tipo,token] = authHeader?.split(' ')
-    console.log("Passei no middleware")
-    next()
-    //return res.status(401).json({mensagem:"Você não tem permissão para acessar esse recurso!"})
+    const token = authHeader.split(' ')[1]!
+    jwt.verify(token,process.env.JWT_SECRET!, (err,decoded)=>{
+        if(err){
+            console.log(err)
+            return res.status(401).json({mensagem:"Token inválido!"})
+        }
+        if(typeof decoded === 'string' || !decoded || !("usuarioId" in decoded))
+            return res.status(401).json({mensagem:"Token inválido!"})
+        
+        req.usuarioId = decoded.usuarioId;
+        next()
+    })
+//     console.log("Passei no middleware")
+//     next()
+//     //return res.status(401).json({mensagem:"Você não tem permissão para acessar esse recurso!"})
 }
 
 export default Auth
